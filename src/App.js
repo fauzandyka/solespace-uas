8ea2eba879342b5f2246c2139ff2e38875d128a5
Fauzan Andyka <102023083+fauzandyka@users.noreply.github.com>
2022-12-23 20:05:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css'
 import NavigationBar from './components/NavigationBar'
 import Intro from './components/Intro'
-import Trending from './components/Trending'
+import Shoes from './components/Shoes'
 import "./style/landingPage.css"
+import Login from './components/Login'
 
 function App() {
   return (
     <div>
       <div className='myBG'>
+        <Login />
         <NavigationBar />
         <Intro />
       </div>  
 
-      <div className='trending'>
-        <Trending />
+      <div className='shoes'>
+        <Shoes />
       </div>
     </div>
   );
